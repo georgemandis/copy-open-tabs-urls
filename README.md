7c86346b283dbe25b8c077b0686a1176f3d86a3e
George Mandis <george@mand.is>
2024-02-10 18:29:14 -0500

    Updated to v2. Updated for Manifest v3, new functionality and README

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
-# Copy Open Tab URLs 
+# Copy URLs
 
-Minimal-fuss Chrome extension to copy the URLs from all your open tabs in a window to the clipbooard.
+Minimal-fuss Chrome extension to copy URLs to your clipboard from various sources including:
+
+- Open tabs in your current window
+- Links within the current page
+- Links within a selection
 
 ## How to use
 
-- Right-click anywhere on the page and select "Copy open URLs" from the context menu
-- If you keep the extension in the Chrome menu you can click the icon to reveal the context menu there as well
+- Right-click anywhere on the page and select "Copy URLs to clipboard from..." from the context menu
+- Select the source you wish to copy from from the sub-menu:
+  - "Links in this selection" will copy links within any selected text on the current page
+  - "Open tabs in this window" will copy the URLs from all the tabs in your current window
+  - "Links on this page" will copy any links found on the page you are currently viewing
+- If you keep the extension pinned in your browser you can right-click the icon to reveal the context menu there as well
 
 That's it!
 
--- background.js
+++ background.js
@@ -1,31 +1,92 @@
 /**
- * Copy open tab URLs to the clipboard
+ * 📋 Copy URLs
  * ===
+ * Quickly copy URLs from: 
+ * - All your open tabs
+ * - Links within the currently active tab
+ * - Links within a selection of text
+ * 
  * George Mandis (george@mand.is)
  * https://george.mand.is
  */
 
-// kind of feels like a strange hack but I guess
-// this is how a Chrome extension copies text to
-// the clipboard in the background?
-const text = document.createElement("textarea");
-document.body.append(text);
-
-function getCurrentTabs() {
-  chrome.tabs.getAllInWindow(async (tabs) => {
-    const urls = tabs.map((value) => value.url);
-    text.value = urls.join("\n");
-    text.select();
-    document.execCommand("copy");
-  });
+function copyURLsToClipboard(urls) {
+  navigator.clipboard.writeText(urls.join("\n"));
 }
 
-chrome.contextMenus.onClicked.addListener(getCurrentTabs);
+function getLinksOnPage() {
+  return Array.from(document.links).map(a => a.href);
+}
 
+function getLinksWithingSelection() {
+  const selection = document.getSelection();
+  const range = selection.getRangeAt(0);
+  const links = range.cloneContents().querySelectorAll("a[href]");
+  return Array.from(links).map(a => a.href);
+}
+
+async function getURLs(req, sender) {  
+  const tabs = await chrome.tabs.query({});
+  const activeTab = tabs.find(t => t.active);
+  let urls;
+
+  if (req?.menuItemId == "copy-open-tab-urls") {
+    urls = tabs.map((value) => value.url);
+  }
+
+  if (req?.menuItemId == "copy-link-urls-within-page") {
+    urls = (await chrome.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      function: getLinksOnPage
+    }))[0].result;
+  }
+
+  if (req?.menuItemId == "copy-link-urls-within-selection") {
+    urls = (await chrome.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      function: getLinksWithingSelection
+    }))[0].result;
+  }
+
+  chrome.scripting.executeScript({
+    target: { tabId: activeTab.id },
+    function: copyURLsToClipboard,
+    args: [urls],
+  });
+
+}
+
+/**
+ * Add handlers for context menu + action clicks
+ */
+chrome.contextMenus.onClicked.addListener(getURLs);
+
+/**
+ * Add our context menus
+ */
 chrome.runtime.onInstalled.addListener(function () {
+  const mainMenuId = "copy-urls";
   chrome.contextMenus.create({
-    title: "Copy open URLs to clipboard",    
-    id: "copy-open-urls",
-    contexts: ["all"]
+    title: "Copy URLs to clipboard from...",
+    id: mainMenuId,
+    contexts: ["page", "action", "selection"]
+  });
+  chrome.contextMenus.create({
+    title: "Links in this selection",
+    id: "copy-link-urls-within-selection",
+    parentId: mainMenuId,
+    contexts: ["selection"]
+  });
+  chrome.contextMenus.create({
+    title: "Open tabs in this window",
+    id: "copy-open-tab-urls",
+    parentId: mainMenuId,
+    contexts: ["page", "action", "selection"]
+  });
+  chrome.contextMenus.create({
+    title: "Links on this page",
+    id: "copy-link-urls-within-page",
+    parentId: mainMenuId,
+    contexts: ["page", "action", "selection"]
   });
 });
